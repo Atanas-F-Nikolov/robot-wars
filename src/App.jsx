c7da2b9ee814f7bef3c6c3f3ea12b11a05b488ca
Atanas-F-Nikolov <a.f.nikolov@gmail.com>
2019-08-11 23:15:10 +0200

    Eslint remove and clean up.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { NavigationBar } from './components/NavigationBar';
