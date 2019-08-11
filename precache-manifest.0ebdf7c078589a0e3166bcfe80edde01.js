d54bc2b54ba9d686bfce4244f4583c1b354d38b5
Atanas-F-Nikolov <a.f.nikolov@gmail.com>
2019-08-11 17:44:05 +0200

    Updates

---
+++ precache-manifest.0ebdf7c078589a0e3166bcfe80edde01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0574dc3ce08924f1814c47ef2fbf805f",
+    "url": "/robot-wars/index.html"
+  },
+  {
+    "revision": "8ef860adbb66ea685f9f",
+    "url": "/robot-wars/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "856dac1c998e69d866ec",
+    "url": "/robot-wars/static/js/2.8e0aa26c.chunk.js"
+  },
+  {
+    "revision": "8ef860adbb66ea685f9f",
+    "url": "/robot-wars/static/js/main.7984d09b.chunk.js"
+  },
+  {
+    "revision": "2f8a8e8b66d66bdf35c9",
+    "url": "/robot-wars/static/js/runtime~main.e7f4175e.js"
+  }
+]);
