9b47fe63a85e8bdf47c22ba72d90b68d377e9864
Atanas-F-Nikolov <a.f.nikolov@gmail.com>
2019-08-14 01:40:34 +0200

    Add a basename.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 import { NavigationBar } from './components/NavigationBar';
 import { Home } from './pages/Home';
 import { RobotList } from './pages/RobotList';
@@ -9,18 +9,20 @@ import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className={styles.wrapper}>
-      <NavigationBar />
-      <main className={styles.main}>
-        <Switch>
-          <Route exact path="/" component={props => <Home {...props} />} />
-          <Route exact path="/robots" component={props => <RobotList {...props} />} />
-          <Route exact path="/robots/:id" component={props => <RobotView {...props} />} />
-          <Route exact path="/404" component={() => <PageNotFound />} />
-          <Route exact component={() => <Redirect to="/404" />} />
-        </Switch>
-      </main>
-    </div>
+    <BrowserRouter basename="/robot-wars">
+      <div className={styles.wrapper}>
+        <NavigationBar />
+        <main className={styles.main}>
+          <Switch>
+            <Route exact path="/" component={props => <Home {...props} />} />
+            <Route exact path="/robots" component={props => <RobotList {...props} />} />
+            <Route exact path="/robots/:id" component={props => <RobotView {...props} />} />
+            <Route exact path="/404" component={() => <PageNotFound />} />
+            <Route exact component={() => <Redirect to="/404" />} />
+          </Switch>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
